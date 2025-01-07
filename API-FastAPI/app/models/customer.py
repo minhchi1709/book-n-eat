0e34174942c5756090395940462be4bf0bc7bd3f
Minh Chi Diep <diepminhchi1617@gmail.com>
2025-01-07 20:34:32 +0100

    FastAPI

---
+++ API-FastAPI/app/models/customer.py
@@ -0,0 +1,10 @@
+﻿from sqlmodel import Field, Session, SQLModel, create_engine, select
+from datetime import date, datetime
+
+class Customer(SQLModel, table=True):
+    id: int | None = Field(None, primary_key=True)
+    email: str = Field(index = True, unique = True)
+    firstname: str
+    lastname: str
+    birthday: date
+    createdAt: datetime
